4deb998f1ed98cfeba2a99e595c4ee4cea405ef3
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-01-23 17:41:11 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,21 @@
+class Day1{
+    public static void main(String[] args){
+
+    }
+    public int maxProfit(int[] prices){
+        int minprize = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length ; i++){
+            if(prices[i] < minprize){
+                minprize = prices[i];
+            }
+            else if(prices[i] - minprize > maxProfit){
+                maxProfit = prices[i] - minprize;
+            
+            }
+        
+
+        }
+        return maxProfit;
+    }
+}
