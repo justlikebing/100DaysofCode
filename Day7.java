5736bc7f3c06e8c21231bf057ffd294cea38f82d
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-01-29 23:12:33 +0530

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,26 @@
+public class Day7 {
+    public static void main(String[] args){
+
+    }
+    class solution{
+        public ListNode removeNthEle(ListNode head, int n){
+            ListNode start = new ListNode();
+            start.next = head;
+            ListNode fast = start;
+            ListNode slow = start;
+
+            for(int i = 1; i <= n ; i++){
+                fast = fast.next;
+            }
+            while(fast != null){
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+
+            return start.next;
+
+        }
+    }
+    
+}
