ba4df32f71ade8a57be8e02b92bb8f03ef1b2d0b
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-01-31 23:14:54 +0530

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,47 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Day9 {
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original Linked List:");
+        printList(head);
+        head = reverseList(head);
+
+        System.out.println("Reversed Linked List:");
+        printList(head);
+    }
+}
