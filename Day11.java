e0187fa1f767f9ff4d673603769f1c8cade223b6
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-02-02 23:42:39 +0530

    Add files via upload

---
+++ Day11.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Day11 {
+    private static int simpleHash(String input) {
+        char[] charArray = input.toCharArray();
+        Arrays.sort(charArray);
+
+        int hash = 0;
+        for (char c : charArray) {
+            hash += (int) c;
+        }
+
+        return hash;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "world";
+
+        int hash1 = simpleHash(str1);
+        int hash2 = simpleHash(str2);
+        System.out.println("Hash Code for \"" + str1 + "\": " + hash1);
+        System.out.println("Hash Code for \"" + str2 + "\": " + hash2);
+    }
+}
