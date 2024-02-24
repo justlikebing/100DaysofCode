ca6500b3a41cd9c9c2ab15151266de197b8e1c60
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-02-24 23:24:32 +0530

    Update

---
+++ Day33.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Day33 {
+    
+    public static int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println("Length of Longest Increasing Subsequence: " + lengthOfLIS(nums));
+    }
+}
