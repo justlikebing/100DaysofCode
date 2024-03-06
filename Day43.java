e24e56e9d8b8e76a90d34570c381ffa94d1a7609
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-03-06 23:29:59 +0530

    Update Day43.java

--- Day43.java
+++ Day43.java
@@ -1,1 +1,32 @@
-Hello
+public class MatrixChainMultiplication {
+
+    public static int matrixChainOrder(int[] arr) {
+        int n = arr.length - 1; 
+        int[][] dp = new int[n][n];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][i] = 0;
+        }
+
+        for (int len = 2; len < n; len++) {
+            for (int i = 1; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    int cost = dp[i][k] + dp[k + 1][j] + arr[i - 1] * arr[k] * arr[j];
+                    if (cost < dp[i][j]) {
+                        dp[i][j] = cost;
+                    }
+                }
+            }
+        }
+
+        return dp[1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {40, 20, 30, 10, 30};
+        int result = matrixChainOrder(arr);
+        System.out.println("Minimum number of multiplications: " + result);
+    }
+}
