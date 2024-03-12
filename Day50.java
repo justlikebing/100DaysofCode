c025e7dff1475e75784dbee62f973a29ac526725
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-03-12 10:19:18 +0530

    Update Day50.java

--- Day50.java
+++ Day50.java
@@ -1,1 +1,49 @@
-hello world 
+public class Day50 {
+    public static void main(String[] args) {
+        int[] logs = { /* Your array of logs */ };
+        int axes = /* Number of axes */;
+        int logCuttingStandCapacity = /* Capacity of log cutting stand */;
+
+        int result = findMinimumMoves(logs, axes, logCuttingStandCapacity);
+        System.out.println("Minimum moves to determine the limit: " + result);
+    }
+
+    private static int findMinimumMoves(int[] logs, int axes, int logCuttingStandCapacity) {
+        int left = 1;
+        int right = logCuttingStandCapacity;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canCutLogs(logs, axes, logCuttingStandCapacity, mid)) {
+                // If logs can be cut with the current mid value, try smaller values
+                right = mid - 1;
+            } else {
+                // If logs cannot be cut with the current mid value, try larger values
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static boolean canCutLogs(int[] logs, int axes, int logCuttingStandCapacity, int mid) {
+        int moves = 0;
+
+        for (int i = 0; i < logs.length; ) {
+            int currentLogs = 0;
+
+            while (i < logs.length && currentLogs + logs[i] <= mid) {
+                currentLogs += logs[i];
+                i++;
+            }
+
+            moves++;
+            if (moves > axes) {
+                return false; // Axe is broken, cannot use it again
+            }
+        }
+
+        return true;
+    }
+}
