55d75c29f2eb3bf6a611c6114e399fd52d763cff
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-03-18 10:25:05 +0530

    Update Day54.java

--- Day54.java
+++ Day54.java
@@ -1,1 +1,63 @@
-hi
+import java.util.*;
+
+public class Day54 {
+    
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        for (int i = 0; i < n - 3; i++) {
+\            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                
+                int left = j + 1, right = n - 1;
+                
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        
+        return quadruplets;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt(); 
+        int TARGET = scanner.nextInt(); 
+        int[] nums = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        
+        List<List<Integer>> result = fourSum(nums, TARGET);
+        
+        System.out.println(result.size());
+        
+        scanner.close();
+    }
+}
