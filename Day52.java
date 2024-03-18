030b18e3a3a03150184a1ca65c2671427eda6c9d
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-03-18 10:20:35 +0530

    Update Day52.java

--- Day52.java
+++ Day52.java
@@ -1,1 +1,48 @@
-Hi
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day52 {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        
+        if (numRows == 0) {
+            return triangle;
+        }
+        
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        triangle.add(firstRow);
+        
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = triangle.get(i - 1);
+            List<Integer> row = new ArrayList<>();
+            
+            row.add(1);
+            
+            for (int j = 1; j < i; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            
+            row.add(1);
+            
+            triangle.add(row);
+        }
+        
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+        int N = 4;
+        PascalTriangle pascalTriangle = new PascalTriangle();
+        List<List<Integer>> result = pascalTriangle.generate(N);
+        
+        // Print the result
+        for (List<Integer> row : result) {
+            for (Integer num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
