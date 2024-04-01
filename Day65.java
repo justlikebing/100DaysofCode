cd9570e81677b1ea1fff5180f2ab6ae8d19c1049
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-04-01 12:44:04 +0530

    Update Day65.java

--- Day65.java
+++ Day65.java
@@ -1,1 +1,34 @@
-.
+public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+    
+    public static void main(String[] args) {
+        EditDistance obj = new EditDistance();
+        String S = "kitten";
+        String T = "sitting";
+        System.out.println("Edit Distance: " + obj.minDistance(S, T)); // Output: 3
+    }
+}
