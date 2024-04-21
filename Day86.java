75da7ca1f92c30fbd50f3ee3b4c756307f1060ac
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-04-21 18:32:30 +0530

    Update Day86.java

--- Day86.java
+++ Day86.java
@@ -1,1 +1,39 @@
-...
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Day86 {
+    public boolean checkSumProperty(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        
+        return isSumPropertyValid(root);
+    }
+    
+    private boolean isSumPropertyValid(TreeNode node) {
+        if (node == null) {
+            return true;
+        }
+        
+        if (node.left == null && node.right == null) {
+            return true;
+        }
+        
+        int sum = 0;
+        if (node.left != null) {
+            sum += node.left.val;
+        }
+        if (node.right != null) {
+            sum += node.right.val;
+        }
+        
+        return (node.val == sum) && isSumPropertyValid(node.left) && isSumPropertyValid(node.right);
+    }
+}
