edfe3e7258d9a44061a8c801c11576f1088f4851
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-05-01 17:25:18 +0530

    Update Day97.java

--- Day97.java
+++ Day97.java
@@ -1,1 +1,28 @@
-.
+public class Day97 {
+    public static int minRepeats(String a, String b) {
+        int repeat = 1;
+        StringBuilder repeatedA = new StringBuilder(a);
+
+        while (repeatedA.length() < b.length()) {
+            repeatedA.append(a);
+            repeat++;
+        }
+
+        if (repeatedA.toString().contains(b)) {
+            return repeat;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        String a1 = "abcd";
+        String b1 = "cdabcdab";
+        System.out.println("Output 1: " + minRepeats(a1, b1));
+
+        String a2 = "a";
+        String b2 = "aa";
+        System.out.println("Output 2: " + minRepeats(a2, b2));
+    }
+}
+
