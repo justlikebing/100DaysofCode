e552d177a93693bc75dfbf3ebcbf9103895d83b8
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-05-01 17:35:17 +0530

    Update Day88.java

--- Day88.java
+++ Day88.java
@@ -1,1 +1,27 @@
-.
+public class Day88 {
+    public static int getXOR(int a, int b) {
+        return a ^ b;
+    }
+
+    public static int getBit(int c, int d) {
+        int mask = 1 << c;
+        return (d & mask) == 0 ? 0 : 1;
+    }
+
+    public static int setBit(int e, int f) {
+        int mask = 1 << e;
+        return f | mask;
+    }
+
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 1, d = 3, e = 2, f = 3;
+        
+        System.out.print(getXOR(a, b) + " ");
+        
+        
+        System.out.print(getBit(c, d) + " ");
+        
+        System.out.println(setBit(e, f));
+    }
+}
+
