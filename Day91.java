1bfd22d8215fa02c2999f3a41cbe35fc790b6f92
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-05-01 17:40:05 +0530

    Update Day91.java

--- Day91.java
+++ Day91.java
@@ -1,1 +1,30 @@
+public class Day91 {
+    public static void recursiveInsertionSort(int[] arr, int n) {
+        // Base case: If array has only one element or is empty, it is already sorted
+        if (n <= 1) {
+            return;
+        }
+
+        recursiveInsertionSort(arr, n - 1);
+
+        int key = arr[n - 1];
+        int j = n - 2;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    public static void main(String[] args) {
+        int N = 7;
+        int[] arr = {2, 13, 4, 1, 3, 6, 28};
+
+        recursiveInsertionSort(arr, N);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
 
