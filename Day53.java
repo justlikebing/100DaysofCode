029efd76b10fb3d2ba76a9c785e3c40f1326a399
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-05-30 13:42:37 +0530

    Update Day53.java

--- Day53.java
+++ Day53.java
@@ -18,7 +18,6 @@ public class Day53 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int T = scanner.nextInt(); 
-        
         for (int t = 0; t < T; t++) {
             int M = scanner.nextInt(); 
             int N = scanner.nextInt();    
