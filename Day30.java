2b369986ec156107bfc9b6714266699a1ee96804
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-05-31 21:53:03 +0530

    Update Day30.java

--- Day30.java
+++ Day30.java
@@ -1,5 +1,4 @@
 public class Day30 {
-
     public static int knapsack(int[] weights, int[] values, int capacity) {
         int n = weights.length;
         int[][] dp = new int[n + 1][capacity + 1];
