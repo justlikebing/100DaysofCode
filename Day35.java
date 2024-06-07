83d4611fc37a91a1ee76fe1756e56ffc6f0fd218
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-06-07 10:30:15 +0530

    Update Day35.java

--- Day35.java
+++ Day35.java
@@ -2,7 +2,6 @@ public class Day35 {
     public static int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
-
         int maxLength = Math.max(v1.length, v2.length);
 
         for (int i = 0; i < maxLength; i++) {
