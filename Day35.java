9d3d9614b1857615479c5e784c3065e59d248ad3
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-06-07 10:30:25 +0530

    Update Day35.java

--- Day35.java
+++ Day35.java
@@ -3,7 +3,6 @@ public class Day35 {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
         int maxLength = Math.max(v1.length, v2.length);
-
         for (int i = 0; i < maxLength; i++) {
             int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
             int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
