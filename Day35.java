5ebf6b96bca607866d66ef5356824263fc614b06
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-06-07 10:30:08 +0530

    Update Day35.java

--- Day35.java
+++ Day35.java
@@ -1,5 +1,4 @@
 public class Day35 {
-
     public static int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
