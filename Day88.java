80ccddeeac7ee4e4e8c9f3671210df7e250823d5
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-06-18 13:26:18 +0530

    Update Day88.java

--- Day88.java
+++ Day88.java
@@ -14,7 +14,6 @@ public class Day88 {
         int a = 1, b = 2, c = 1, d = 3, e = 2, f = 3;
         System.out.print(getXOR(a, b) + " ");
         System.out.print(getBit(c, d) + " ");
-        
         System.out.println(setBit(e, f));
     }
 }
