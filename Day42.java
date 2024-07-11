ef6827fbee17ff413bb3f65eac61d5b3f9c6fa32
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-07-11 22:46:43 +0530

    Update Day42.java

--- Day42.java
+++ Day42.java
@@ -10,7 +10,6 @@ class Celebrity {
                 candidate = i;
             }
         }
-
         for (int i = 0; i < N; i++) {
             if (i != candidate && (knows(candidate, i) || !knows(i, candidate))) {
                 return -1; // No celebrity found
