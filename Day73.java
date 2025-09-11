a34ec297e0cdf559d51c33efb0fe5e4f95d89f47
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2025-09-11 10:44:11 +0530

    Update Day73.java

--- Day73.java
+++ Day73.java
@@ -7,20 +7,17 @@ class TreeNode {
         this.left = this.right = null;
     }
 }
-
 public class Day73 {
 
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
         return isMirror(root.left, root.right);
     }
-
     private boolean isMirror(TreeNode left, TreeNode right) {
         if (left == null && right == null) return true;
         if (left == null || right == null) return false;
         return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
     }
-
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
